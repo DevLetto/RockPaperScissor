f02cd5fc8240bc1ec19d21d40cfaa6018925497b
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2025-07-02 21:04:20 -0300

    Changes

--- game/play/script.js
+++ game/play/script.js
@@ -9,7 +9,7 @@ function startGame(){
     counter.classList.add("counter")  
 
     counterContainer.appendChild(counter);
-    counterContainer.removeChild(startBtn)
+    startBtn.style.display = "none"
 
    let initialTime = 3;
    controlTimer(initialTime, counter, counterContainer);
@@ -17,18 +17,39 @@ function startGame(){
 
 }
 
-function controlTimer(initialTime, counter, counterContainer){
+function controlTimer(initialTime, counter, counterContainer, startBtn){
     let remainingTime = initialTime;
 
     counter.innerText = remainingTime
 
+    let handImgs = document.querySelectorAll('.handImg')
+    
+    handImgs.forEach((img) => {
+        img.classList.add("animation")
+
+    })
+
     let timer =  setInterval(function(){
         remainingTime--;
         counter.innerText = remainingTime;
+
+        console.log(handImgs)
+        
         if(remainingTime < 1){
             clearInterval(timer);
             counter.innerText = "Go!"
-            counter.style.display = "none"
+
+            handImgs.forEach((img) => {
+                img.classList.remove("animation")
+
+            })
+
+            setTimeout(() => {
+                counter.style.display = "none"
+                startBtn.style.display = "block"
+                
+            },500)
         }
     },500)
+    
 }
