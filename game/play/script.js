38bb6d66b9425d22fb9342c2e0b700fd7d9e3670
Gabriel Letto <158781940+DevLetto@users.noreply.github.com>
2025-07-07 20:05:45 -0300

    Contador adicionado

--- game/play/script.js
+++ game/play/script.js
@@ -1,6 +1,8 @@
-
-const cpuScore = document.querySelector('.rScore').value
-const playerScore = document.querySelector('.yScore').value
+//variaveis
+let cpuScoreBoard = document.querySelector('.rScore')
+let cpuScore = Number(cpuScoreBoard.value)
+const playerScoreBoard = document.querySelector('.yScore')
+let playerScore = Number(playerScoreBoard.value)
 const optBtns = document.querySelectorAll('.btnImg')
 const playerResult = document.querySelector('.rightHandImg')
 const cpuResult = document.querySelector('.leftHandImg')
@@ -10,27 +12,61 @@ showRes.classList.add('showRes')
 let handImgs = document.querySelectorAll('.handDiv')
     
     
-
+//loop para acessar os botoes
 optBtns.forEach((img, index) => {
+    //add evento click em cada botao
     img.addEventListener("click", () => {
+        //add a classe active para que quando apertar o botao ele ficar colorido
         img.classList.add('active')
 
         playerResult.src = "/game/imagens/Mao fechada invertido.png"
         cpuResult.src = "/game/imagens/Mao fechada.png"
+        showRes.innerHTML = ""
 
+        centerContainer.appendChild(showRes); // força o elemento a aparecer no DOM logo
+        // showRes.textContent = 3;  
+        
+        let tempo = 3;
+        showRes.textContent = tempo; // mostra "3" imediatamente
+        centerContainer.appendChild(showRes); // adiciona à tela se ainda não estiver
+
+        const intervalo = setInterval(() => {
+            showRes.textContent = tempo
+            console.log(tempo);
+            tempo--;
+
+            if(tempo === 0){
+                clearInterval(intervalo)
+            }
+            
+            
+        }, 500);
+
+
+        //outro loop para ecessar os botoes
         optBtns.forEach((img2, index2) => {
 
             index !== index2 && img2.classList.remove('active')
 
         })
 
+
+
+
+
+        //add animação nas mãos
         handImgs.forEach((img) => {
             img.classList.add("animation")
 
         })
 
+        
+        
 
+        
         let time = setTimeout(() => {
+
+             
 
             handImgs.forEach((img) => {
                 img.classList.remove("animation")
@@ -66,7 +102,18 @@ optBtns.forEach((img, index) => {
             showRes.textContent = outComeValue
             centerContainer.appendChild(showRes)    
 
-        },2500)
+            if(outComeValue === "Cpu"){
+                cpuScore++
+                cpuScoreBoard.value = Number(cpuScore)
+                
+            }else if (outComeValue === "User"){
+                playerScore++
+                playerScoreBoard.value = Number(playerScore)
+            }
+
+        },2000)
+    
+        
 
         
     })
